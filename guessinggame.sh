e28c058da20f347a1f98d4a4a2b5a229fe377650
Daniel Pringle <daniel@montoux.com>
2019-06-24 21:34:09 +1200

    tweak guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 #
-# Ask the user to guess number of files in the present directory
+# Ask the user to guess number of files in the current directory
 # Congratulate if correct
 # Advise if too low or too high, and ask for another guess
 # Run with: $ bash guessinggame.sh
 
 nfiles=$( ls | wc -l)
 correct=0
-read -p "Guess the number of files in this directory: "  nguess
+read -p "Guess the number of files in the current directory: "  nguess
 
 while [[ $correct -eq 0 ]]
 do
